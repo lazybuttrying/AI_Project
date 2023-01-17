237ac8d05bc90412c6017348682c6a6e21e99246
lazybuttrying <79500842+lazybuttrying@users.noreply.github.com>
2023-01-17 18:02:09 +0900

    sudoku

--- RL/DRLtoTS/log.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(
-    format="%(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
-    datefmt='%Y-%m-%d:%H:%M:%S',
-    level=logging.INFO
-)
-
-LOGGER = logging.getLogger()
