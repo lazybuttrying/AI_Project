39c96ea5a743d8eb692c4a8960466b486715d8b4
DYK <79500842+lazybuttrying@users.noreply.github.com>
2023-03-16 21:24:10 +0900

    edit cuda version

--- swin_transformer/build.sh
+++ swin_transformer/build.sh
@@ -1,4 +1,7 @@
 # ~/바탕화면/dataset_CVAT/surprise
 
+# Check to CUDA in docker container
+# python -c "import torch; torch.cuda.get_device_name(0)"
+
 docker build -t mmdetection docker/
-docker run --gpus all --shm-size=8g -it -v /home/agtech-research/바탕화면/dataset_CVAT/surprise/mmdetection/dataset:/mmdetection/dataset mmdetection
+docker run --gpus all --shm-size=16g -it -v /home/agtech-research/바탕화면/dataset_CVAT/surprise/mmdetection/dataset:/mmdetection/dataset mmdetection
